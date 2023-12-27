39f35f6748bbfdef557d00512daa4c2e5dff1fec
justnotnik <yannik98@yandex.ru>
2023-12-27 16:02:19 +0300

    добавлен вызов докера

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import os.path
+import shutil
+import threading
 from typing import Union
 import uvicorn
 from fastapi import FastAPI, Form, Request
@@ -9,6 +11,10 @@ from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+#globals
+mutex = threading.Lock()
+task_id = 0
 
 file_extensions = {
     "cpp": ".cpp",
@@ -27,18 +33,32 @@ def read_root(request: Request):
 
 
 
-
+def update_id():
+    global task_id
+    task_id += 1
+    return task_id
 
 @app.post("/post_code")
-def post_code(stud_id: Union[int, None] = None, task_id: Union[int, None] = None, code=Form(), lang=Form()):
+def post_code(stud_id: Union[int, None] = None, code=Form(), lang=Form()):
     stud_id = 123
-    task_id = "1123"
-    path = f"./works/{stud_id}"
+    problem_id = 123
+    global task_id
+    mutex.acquire()
+    id = update_id()
+    mutex.release()
+    path = f"./data/works/{id}"
     is_exist = os.path.exists(path)
     if not is_exist:
         os.makedirs(path)
+
     file = open(f"{path}/main{file_extensions[lang]}", 'w')
     file.write(code)
+    file.close()
+    print(f"docker build "
+              f"--build-arg lang={lang} "  
+              f"--build-arg task_id={id} "
+              f"--build-arg problem_id={problem_id}")
+    shutil.rmtree(path)
     return RedirectResponse("/op", status_code=302)
 
 
