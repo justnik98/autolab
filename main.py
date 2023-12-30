a9134640872c981c489b94a9530689d92d1c0066
justnotnik <yannik98@yandex.ru>
2023-12-30 15:37:24 +0300

    Добавлена отправка ответа с результатом теста

--- main.py
+++ main.py
@@ -6,13 +6,14 @@ from typing import Union
 import uvicorn
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-#globals
+# globals
 mutex = threading.Lock()
 task_id = 0
 
@@ -28,9 +29,8 @@ def read_root(request: Request):
 
 
 @app.get("/op")
-def read_root(request: Request):
+async def op(request: Request):
     return templates.TemplateResponse("op.html", {"request": request})
-
 
 
 def update_id():
@@ -38,8 +38,9 @@ def update_id():
     task_id += 1
     return task_id
 
-@app.post("/post_code")
-def post_code(stud_id: Union[int, None] = None, code=Form(), lang=Form()):
+
+@app.post("/post_code", response_class=HTMLResponse)
+async def post_code(request: Request, stud_id: Union[int, None] = None, code=Form(), lang=Form()):
     stud_id = 123
     problem_id = 123
     global task_id
@@ -54,19 +55,28 @@ def post_code(stud_id: Union[int, None] = None, code=Form(), lang=Form()):
     file = open(f"{path}/main{file_extensions[lang]}", 'w')
     file.write(code)
     file.close()
-    print(f"docker build "
-              f"--build-arg lang={lang} "  
-              f"--build-arg task_id={id} "
-              f"--build-arg problem_id={problem_id}"
-              f"-t {id}")
-    print(f"docker run {id} --rm &> {id} && docker rmi -f {id}")
-
-    shutil.rmtree(path)
-    return RedirectResponse("/op", status_code=302)
-
-
-def run_tests(params: json, k: int):
-    return 0
+    # os.system(f"docker build ./scripts/"
+    #           f"--build-arg lang={lang} "
+    #           f"--build-arg task_id={id} "
+    #           f"--build-arg problem_id={problem_id}"
+    #           f"-t {id}")
+    # os.system(f"docker run {id} --rm &> {path}/{id}err > {path}/{id} && docker rmi -f {id}")
+    print(f"docker build ./scripts/"
+          f"--build-arg lang={lang} "
+          f"--build-arg task_id={id} "
+          f"--build-arg problem_id={problem_id}"
+          f"-t {id}")
+    print(f"docker run {id} --rm {id} --rm &> {path}/{id}err > {path}/{id} && docker rmi -f {id}")
+    file = open(f"{path}/out", 'r')
+    # print(file.read())
+    output = file.read()
+    file.close()
+    # shutil.rmtree(path)
+    result = ""
+    result = "Not Passed"
+    return templates.TemplateResponse(name="op.html",
+                                      context={"request": request, "id": id, "result": result, "output": output})
+    # return RedirectResponse("/op", status_code=302)
 
 
 if __name__ == '__main__':
