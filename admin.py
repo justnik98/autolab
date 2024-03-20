64051fe786c5d638650b529193826777c75474f6
Nikita Yankovskii <yannik98@yandex.ru>
2024-03-20 21:31:51 +0300

    в админ панель добавлена возможность добавлени преподов

--- admin.py
+++ admin.py
@@ -15,7 +15,7 @@ async def admin_panel(request: Request):
 user_id = 0
 
 
-@app.post("/add_student", dependencies=[Depends(get_admin_user)])
+@app.post("/add_student", dependencies=[Depends(get_admin_user)], status_code=201)
 async def add_student(data=Body()):
     username = data['username']
     password = data['password']
@@ -33,7 +33,31 @@ async def add_student(data=Body()):
     cur.execute(f"INSERT INTO students (user_id, surname, first_name, patronymic, group_num, email) "
                 f"VALUES({user_id}, '{surname}' , '{name}' ,'{patronymic}', {group} ,'{email}')")
     db.commit()
-    return
+
+
+@app.post("/add_teacher", dependencies=[Depends(get_admin_user)], status_code=201)
+async def add_student(data=Body()):
+    username = data['username']
+    password = data['password']
+    surname = data['surname']
+    name = data['first_name']
+    patronymic = data['patronymic']
+    email = data['email']
+    position = data['position']
+    department = data['department']
+
+    cur = db.cursor()
+    cur.execute(f"SELECT id from positions WHERE position = '{position}'")
+    pos_id = cur.fetchone()[0]
+    cur.execute(f"SELECT id from departments WHERE department = '{department}'")
+    dep_id = cur.fetchone()[0]
+    cur.execute(f"INSERT INTO auth (username, password, role_id) "
+                f"VALUES('{username}', '{pwd_context.hash(password)}', 0) RETURNING id")
+    global user_id
+    user_id = cur.fetchone()[0]
+    cur.execute(f"INSERT INTO teachers (user_id, surname, first_name, patronymic, email, position_id, departmnet_id) "
+                f"VALUES({user_id}, '{surname}' , '{name}' ,'{patronymic}', '{email}, {pos_id}, {dep_id}')")
+    db.commit()
 
 
 # TODO: сделать чтение данных из json
