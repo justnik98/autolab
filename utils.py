07d62facd2794a7b5c09c22140d2745a9ebcfe81
Nikita Yankovskii <yannik98@yandex.ru>
2024-03-23 22:27:21 +0300

    исправлен поиск студентов

--- utils.py
+++ utils.py
@@ -2,6 +2,14 @@ from auth import *
 from fastapi import Body
 
 
+class Student(BaseModel):
+    name: str | None
+    surname: str | None
+    patronymic: str | None
+    group: str | None
+    email: str | None
+
+
 @app.get("/getall_groups", dependencies=[Depends(get_admin_user)])
 async def getall_groups():
     res = []
@@ -32,25 +40,29 @@ async def getall_groups():
     return res
 
 
-@app.get("/search_students", dependencies=[Depends(get_admin_user)])
+@app.post("/search_students", dependencies=[Depends(get_admin_user)])
 async def search_students(data=Body()):
     res = []
     surname = f"{data['surname']}%"
     name = f"{data['first_name']}%"
     patronymic = f"{data['patronymic']}%"
-    group = f"{data['group']}%"
-
+    group = data['group']
+    group_id = -1
     cur = db.cursor()
-    cur.execute(f"SELECT id from groups WHERE group_num = '{group}'")
-    group_id = cur.fetchone()[0]
-
-    cur.execute(f"SELECT students WHERE "
-                f"surname LIKE {surname} AND "
-                f"first_name LIKE {name} AND "
-                f"patronymic LIKE {patronymic} AND "
-                f"group_id LIKE {group_id}")
-
+    cmd = f"SELECT * FROM students WHERE " \
+          f"surname LIKE '{surname}' AND " \
+          f"first_name LIKE '{name}' AND " \
+          f"patronymic LIKE '{patronymic}'"
+    if group != "":
+        cur.execute(f"SELECT id from groups WHERE group_num = '{group}'")
+        group_id = cur.fetchone()[0]
+        cmd = f"{cmd} AND group_id = {group_id}"
+    cur.execute(cmd)
+    cur2 = db.cursor()
     for row in cur:
-        res.append(row)
-        print(row)
+        cur2.execute(f"SELECT group_num from groups WHERE id = '{row[6]}'")
+        group = cur2.fetchone()[0]
+        s = {'surname': row[2], 'name': row[3], 'patronymic': row[4], 'email': row[5], 'group': group}
+        res.append(s)
+        print(s)
     return res
